aff07261be5074b6953a30c813680273cdc2ea99
fabiocerioni <fabio.cerioni@gmail.com>
2024-05-29 08:59:28 +0200

    first commit

---
+++ srv/world.cds
@@ -0,0 +1,4 @@
+service say {
+  function hello (to:String) returns String;
+  action add (x:Integer, to: Integer) returns Integer;
+}
---
+++ srv/world.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fesr = require("@sap/fesr-to-otel-js");
+const app = express();
+module.exports = async function () {
+    this.on('hello', async (req) => {
+        return `Hello ${req.data.to}!`;
+    });
+    this.on('add', async (req) => {
+         return req.data.x + req.data.to })
+    this.on('bootstrap', async (req) => {
+        fesr.registerFesrEndpoint(app);})
+            
+    }
